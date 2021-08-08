789e8816b460de88b0d048eeadafd2b80cd24842
Raunak Madan <37419003+Coderest17@users.noreply.github.com>
2021-08-07 21:43:39 -0400

    Python script to analyze Zoom attendance reports.

--- AttendanceChecker.py
+++ AttendanceChecker.py
@@ -6,8 +6,17 @@ to view the attendance as well as duration of the participants.
 #Importing pandas library
 import pandas as pd
 
-#Importing datasets
-attendance_dataset = pd.read_csv('Zoom Attendance.csv')
+#Importing system libraries
+import os
+import sys
+import warnings
+
+#Checking length of input arguments
+if len(sys.argv) != 3:
+    sys.exit("\nUsage: python Attendance_Checker.py Zoom-Attendance-CSV-Directory Class-List-CSV-Directory.csv\n")
+
+#Importing Zoom attendance dataset
+attendance_dataset = pd.read_csv(sys.argv[1])
 
 #Removing incorrect attendance header
 attendance_dataset.columns = ["" for col in attendance_dataset]
@@ -32,13 +41,16 @@ del attendance_revised["Guest"]
 #Renaming columns
 attendance_revised.columns = ["Name", "Duration (Min)"]
 
-#Importing class list
-class_list = pd.read_csv('Class List.csv') #Make sure it is a 1 column dataset with header (Ex: "Students")
+#Importing class list dataset
+class_list = pd.read_csv(sys.argv[2])
 
 #Adding "Present" and "Duration" columns to dataset, and setting all values to 0
 class_list["Present"] = [0 for i in range(len(class_list))]
 class_list["Duration"] = [0 for i in range(len(class_list))]
 class_list.columns = ["Student", "Present", "Duration (Min)"]
+
+#Hiding warning
+warnings.filterwarnings("ignore")
 
 #Iterating through attendance data to check if all students were there
 for i in range(len(class_list)):
@@ -48,8 +60,19 @@ for i in range(len(class_list)):
   else:
     class_list["Present"][i] = 0
 
-#Sorting dataframe based on duration length
+#Sorting dataframe based on duration
 class_list = class_list.sort_values(by=["Duration (Min)"])
 
 #View results
-print(class_list)
+print("\n\t\t\t\t\tAttendance Comparison Results")
+print("The following students were absent:")
+
+missing_count = 0
+for i in range(len(class_list)):
+  if class_list.loc[i][1] == 0:
+    missing_count += 1
+    print(f"{missing_count}. {class_list.loc[i][0]}")
+
+print("\n\tOverall Summary:")
+print(class_list.to_string(index=False))
+print("\n")
